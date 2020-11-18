3abb54a5c215b71816b9f6cea801b6407352c512
JSidat <JSidat95@gmail.com>
2020-11-18 18:14:00 +0000

    add comments

--- FullGame/Ship.java
+++ FullGame/Ship.java
@@ -2,9 +2,10 @@ package FullGame;
 import java.util.*;
 
 public class Ship {
-    private ArrayList<String> locationCells;
-    private String name;
+    private ArrayList<String> locationCells; // array lsit of cell locations
+    private String name; // name of the ship
 
+    // setter method that updates the ship's location(random location provided by the gameHelper placeShip method)
     public void setLocationCells(ArrayList<String> loc) {
         locationCells = loc;
     }
@@ -15,18 +16,18 @@ public class Ship {
 
     public String checkYourself(String userInput) {
         String result= "miss";
-        int index = locationCells.indexOf(userInput);
+        int index = locationCells.indexOf(userInput); // if user guess is one of the entries in the arrayList, indexOf will return its arrayList location, if not it will return -1
         if (index >= 0) {
-            locationCells.remove(index);
+            locationCells.remove(index); // using arrayList's remove method to delete the entry
 
-        if (locationCells.isEmpty()) {
+        if (locationCells.isEmpty()) { // use the empty method in arrayList to see if all of the locations have been guessed
             result = "kill";
-            System.out.println("Ouch! You sunk " + name);
+            System.out.println("Ouch! You sunk " + name); // tell the user when the ship has been sunk
         } else {
             result = "hit";
         }
         }
-        return result;
+        return result; // return "miss" or "hit" or "kill"
     }
 }
 
